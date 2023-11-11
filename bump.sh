20c91e8811dd41e59f93b4f599b8cc1d1e840d90
Andrew Johnston <apjohnsto@gmail.com>
2023-11-11 01:38:49 +0000

    v8 release 12.0

--- bump.sh
+++ bump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 RELEASE=12.0
-#git push --delete origin $RELEASE
+git tag --delete $RELEASE
+git push --delete origin $RELEASE
 echo v8 release $RELEASE >> CHANGELOG.md
 git commit -a -m "v8 release $RELEASE"
 git tag $RELEASE
