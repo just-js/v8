efeda040dfd11bc5ea07aab082a55c13481f3ac4
Andrew Johnston <apjohnsto@gmail.com>
2023-12-19 13:59:48 +0000

    use zip archives on windows build

--- bump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-RELEASE=12.0
-git tag --delete $RELEASE
-git push --delete origin $RELEASE
-git commit -a -m "v8 release $RELEASE"
-git tag $RELEASE
-git push origin main --force --tags
